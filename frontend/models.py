9657f14f11143ea7a18aaed8e98d27e0a9662639
lesley ayemi <leeayemi@gmail.com>
2021-07-20 22:38:36 +0100

    model intial setup

--- frontend/models.py
+++ frontend/models.py
@@ -22,12 +22,13 @@ class Shipment(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=5)
     insurance = models.CharField(max_length=50, choices=INSURANCE)
+    quantity = models.IntegerField(min)
     status = models.CharField(max_length=250, choices=STATUS, default='pending')
     date_order = models.DateTimeField(auto_now_add=True)
 
 
 class Track(models.Model):
 
-    shipment_id = models.ForeignKey(Shipment)
+    shipment = models.ForeignKey(Shipment)
     address = models.CharField(max_length=225, blank=True)
     
