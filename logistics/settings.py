515f6f8560ecf89cba6fcfcfa571c60ffa29e3dc
lesley ayemi <leeayemi@gmail.com>
2021-07-21 23:10:17 +0100

    minor changes

--- logistics/settings.py
+++ logistics/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'logistics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': env('DATABASE_TYPE'),
         'NAME': env('DATABASE_NAME'),
         'USER': env('DATABASE_USER'),
         'PASSWORD': env('DATABASE_PASSWORD'),
